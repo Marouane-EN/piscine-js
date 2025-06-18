4cdaf0b99a2f82d76c4d1a7e09b415349c4a39a6
Your Name <you@example.com>
2025-06-18 11:35:09 +0100

    final commit

---
+++ min-max.js
@@ -0,0 +1,13 @@
+const max = (num1, num2) => {
+    if (num1 > num2) {
+        return num1
+    }
+    return num2
+}
+
+const min = (num1, num2) => {
+    if (num1 < num2) {
+        return num1
+    }
+    return num2
+}
