4f8dcb5f19aa077282d27a23de103872ac73b34f
Your Name <you@example.com>
2025-06-18 09:18:31 +0100

    first commit

---
+++ declarations.js
@@ -0,0 +1,18 @@
+const escapeStr = '`\/"\''
+
+const arr = [4,'2']
+const obj = Object.freeze({
+    str : 'string',
+    num : 0,
+    bool : true,
+    undef : undefined
+})
+
+const nested = Object.freeze({
+    arr : [4,undefined,'2'],
+    obj : {
+        str : '',
+        num : 0,
+        bool : true
+    }
+})
