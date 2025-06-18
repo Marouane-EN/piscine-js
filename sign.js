cf33605aaf9ffb90447dbe1b744f32edde3870bb
Your Name <you@example.com>
2025-06-18 11:57:38 +0100

    final commit

--- sign.js
+++ sign.js
@@ -1,4 +1,4 @@
-const sing = (num) => (num > 0 ? 1 : num < 0 ? -1 : 0);
+const sign = (num) => (num > 0 ? 1 : num < 0 ? -1 : 0);
 
 const sameSign = (num1, num2) =>
   num1 === 0 || num2 === 0 ? num1 === num2 : num1 * num2 > 0 ? true : false;
