c4297f4c14406c8eb4b07196679b8a11ac6d4928
Your Name <you@example.com>
2025-06-18 09:34:17 +0100

    final commit

---
+++ more-or-less.js
@@ -0,0 +1,12 @@
+function more(params) {
+    return params++
+}
+function less(params) {
+    return params--
+}
+function add(params, arg) {
+    return params + arg
+}
+function more(params, arg) {
+    return params - arg
+}
