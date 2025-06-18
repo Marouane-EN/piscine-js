7fa8a2089e965c15206b67842e15baedf1a0e89c
Your Name <you@example.com>
2025-06-18 11:29:19 +0100

    final commit

--- abs.js
+++ abs.js
@@ -1,7 +1,7 @@
-const isPositive = (n) => n >= 0
+const isPositive = (n) => n > 0
 
 const abs = (n) => {
-    if (!isPositive(n)) {
+    if (!isPositive(n) && n != 0) {
         return n * -1
     }
     return n
