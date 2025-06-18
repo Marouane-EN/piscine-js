48f423544a787e07ef59056d426c2bc7288ae85f
Your Name <you@example.com>
2025-06-18 09:28:52 +0100

    final commit

---
+++ mutability.js
@@ -0,0 +1,14 @@
+const clone1 = {
+    name: '',
+    age: 0,
+    country: ''
+}
+const clone2 = {
+    name: '',
+    age: 0,
+    country: ''
+}
+let samePerson = person
+
+samePerson.age++
+samePerson.country = 'FR'
