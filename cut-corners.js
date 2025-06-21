1a942b7c427d206a183673f9f1cbc3814bc338a6
Your Name <you@example.com>
2025-06-21 12:43:09 +0100

    final commit

---
+++ cut-corners.js
@@ -0,0 +1,87 @@
+const round = (n) => {
+  const negative = n < 0;
+  let num = 0;
+  if (negative) {
+    n *= -1;
+  }
+  while (num < n) {
+    num++;
+  }
+  if (n == num) {
+    return n;
+  }
+  if (n <= num - 0.5 && !negative) {
+    return num - 1;
+  }
+  if (n > num - 0.5 && !negative) {
+    return num;
+  }
+  if (n > num - 0.5 && negative) {
+    return num * -1;
+  }
+
+  return num * -1 + 1;
+};
+
+const ceil = (n) => {
+  const negative = n < 0;
+  let num = 0;
+  if (negative) {
+    n *= -1;
+  }
+  while (num < n) {
+    num++;
+  }
+  if (n == num) {
+    return n;
+  }
+  if (!negative) {
+    return num;
+  }
+
+  return num * -1 + 1;
+};
+
+const floor = (n) => {
+  const negative = n < 0;
+  let num = 0;
+
+  if (negative) {
+    n *= -1;
+  }
+  while (num < n) {
+    num++;
+  }
+  if (n == num) {
+    return n;
+  }
+  if (!negative) {
+    return num - 1;
+  }
+
+  return num * -1;
+};
+
+const trunc = (n) => {
+  n = Number(n);
+  const negative = n < 0;
+  let num = 0;
+
+  if (negative) {
+    n *= -1;
+  }
+  if (n > 68719476735) {
+    return 0;
+  }
+  while (num < n) {
+    num++;
+  }
+  if (n == num) {
+    return n;
+  }
+  if (!negative) {
+    return num - 1;
+  }
+
+  return num * -1 + 1;
+};
