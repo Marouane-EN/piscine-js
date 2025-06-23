c25937f57a9c9c64708f82ca7feabeed66475d70
Your Name <you@example.com>
2025-06-23 15:34:21 +0100

    first commit

--- group-price.js
+++ group-price.js
@@ -2,6 +2,7 @@ const groupPrice = (s) => {
   const regx = /(USD|\$)(\d+)\.(\d+)/g;
   let array = [];
   const result = [];
+  let match;
   while ((match = regx.exec(s)) !== null) {
     array.push(match[0], match[2], match[3]);
     result.push(array);
