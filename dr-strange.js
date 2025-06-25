2f619f26f3c4ba6bbaf9ad4ccd1337fface253f1
soubaaiss <aissoulhcen@gmail.com>
2025-06-25 17:34:44 +0100

    final commit

--- dr-strange.js
+++ dr-strange.js
@@ -1,8 +1,17 @@
 const addWeek = (date) => {
-    let day = date.getUTCDate()
+
+    let today = new Date("0001-01-01");
+    // console.log(typeof today);
+
+    // let day = date.getUTCDate()
+    let day = (date - today) / 1000 / 3600 / 24;
+    // console.log(day);
+
     day = day % 14
+    // console.log(day+1);
+
     const arr = ["secondSunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "secondMonday", "secondTuesday", "secondWednesday", "secondThursday", "secondFriday", "secondSaturday"]
-    return arr[day];
+    return arr[day+1];
 }
 
 const timeTravel = (timeObject) => {
@@ -11,3 +20,5 @@ const timeTravel = (timeObject) => {
 
 
 }
+
+console.log(addWeek(new Date('2025-08-11')));
