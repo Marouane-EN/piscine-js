1b7d9ec3f03bb8da92e34db27d8bb76d71e1b679
mennaas <mennaas@example.com>
2025-06-26 13:02:02 +0100

    final commit

--- get-some-time.js
+++ get-some-time.js
@@ -1,23 +1,52 @@
+function gitDate(date) {
+    let s = ""
+    if (date.getDate() < 10) {
+        s += "0" + date.getDate() + "-"
+    } else {
+        s += date.getDate() + "-"
+    }
+    if (1 + date.getMonth() < 10) {
+        s += "0" + (1 + date.getMonth()) + "-"
+    } else {
+        s += (1 + date.getMonth()) + "-"
+    }
+    if (date.getFullYear() < 1000) {
+        s += "0" + date.getFullYear()
+    } else {
+        s += date.getFullYear()
+    }
+    
+    return s
+}
 function firstDayWeek(num, year) {
     //let add = 0
-    if (year < 1914) {
-        return "01" + "-" + "01" + "-" + year
-    }
+    // if (year < 1914) {
+    //     return "01" + "-" + "01" + "-" + year
+    // }
     const date = new Date(year, 0, 1)
+    if (num == 1) {
+        
+        return gitDate(date)
+    }
     //console.log(date);
 
     let day = date.getDay()
+    //console.log("day", day);
+
+    let weekToDays = ((num - 1) * 7)
     if (day > 1) {
-        day = 9 - day
-    } else {
-        day = 1
+        weekToDays -= day - 2
+    } else if (day < 1) {
+        weekToDays -= 7
     }
-    const weekToDays = (num - 1) * 7
-    const newDate = new Date(year, 1, weekToDays + day)
-    const s = newDate.getDate().toString() + "-" + newDate.getMonth().toString() + "-" + newDate.getFullYear().toString()
-    return s
+    //console.log(weekToDays);
+
+    const newDate = new Date(year, 0, weekToDays)
+    //console.log(newDate);
+
+    return gitDate(newDate)
 }
 
-// console.log(firstDayWeek(1, "1970"));
+//console.log(firstDayWeek(52, "1000"));
 
 // Math.trunc
