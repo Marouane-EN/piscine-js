cb5db001c9e28996df2b2fae65d4782cc81c618a
soubaaiss <aissoulhcen@gmail.com>
2025-06-27 15:09:23 +0100

    final commit

---
+++ using-map.js
@@ -0,0 +1,30 @@
+const citiesOnly = (array) => array.map((element) => element.city);
+
+const upperCasingStates = (array) =>
+  array.map((element) => element.charAt(0).toUpperCase() + element.slice(1));
+
+const fahrenheitToCelsius = (array) =>
+  array.map((element) => {
+    const regx = /\d+/;
+
+    const arr = element.match(regx);
+    const cel = (Number(arr[0]) - 32) * (5 / 9);
+    return String(Math.round(cel)) + "°C";
+  });
+
+const trimTemp = (array) =>
+  array.map((element) => {
+    element.temperature = element.temperature.replaceAll(" ", "");
+    return element;
+  });
+
+const tempForecasts = (array) =>
+  array.map((element) => {
+    let s = "";
+    element.temperature = trimTemp([element])[0].temperature;
+
+    const arr = fahrenheitToCelsius([element.temperature]);
+    s += arr[0] + "elsius in " + element.city + ", ";
+    s += upperCasingStates([element.state]);
+    return s;
+  });
