2497d21c6531bd5f316b9eaa17a2ca9c8163764a
soubaaiss <aissoulhcen@gmail.com>
2025-06-27 18:47:29 +0100

    final commit

---
+++ using-reduce.js
@@ -0,0 +1,21 @@
+const adder = (array, initialValue = 0) => {
+  return array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  );
+};
+
+const sumOrMul = (array, initialValue = 0) =>
+  array.reduce(
+    (accumulator, currentValue) =>
+      currentValue % 2 == 0
+        ? accumulator * currentValue
+        : accumulator + currentValue,
+    initialValue
+  );
+
+const funcExec = (array, initialValue) =>
+  array.reduce(
+    (accumulator, currentValue) => accumulator(currentValue()),
+    initialValue
+  );
