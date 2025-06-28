4b54e6ca6e8c797cff4dcf9958253ae5bcc000cc
soubaaiss <aissoulhcen@gmail.com>
2025-06-28 10:29:55 +0100

    final commit

--- for-each.js
+++ for-each.js
@@ -1,5 +1,13 @@
-function forEach(array, func) {
+function forEach(array, func, index, arr) {
   for (let i = 0; i < array.length; i++) {
-    func(array[i]);
+    if (index != undefined && arr != undefined) {
+      func(array[i], index, arr);
+    } else if (index != undefined) {
+      func(array[i], index);
+    } else if (arr != undefined) {
+      func(array[i], arr);
+    } else {
+      func(array[i]);
+    }
   }
 }
