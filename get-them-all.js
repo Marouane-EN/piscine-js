65f6f936678550efe143667eb85360062af9d617
soubaaiss <aissoulhcen@gmail.com>
2025-06-28 15:56:22 +0100

    final commit

--- get-them-all.js
+++ get-them-all.js
@@ -1,28 +1,30 @@
 export function getArchitects() {
-  const atag = document.getElementsByTagName("a");
-  const notatag = document.getElementsByTagName("span");
-  //console.log(atag,notatag);
+  const atag = document.querySelectorAll("a");
+  const notatag = document.querySelectorAll("span");
+  //   console.log(atag,notatag);
 
-  return atag, notatag;
+  return [atag, notatag];
 }
 export function getClassical() {
-  const classe1 = document.getElementsByClassName("classical");
+  const classe1 = document.querySelectorAll("a.classical");
   const notclasse1 = document.querySelectorAll(":not(.classical)");
-  //console.log(classe1, notclasse1);
+  //   console.log(classe1, notclasse1);
 
-  return classe1, notclasse1;
+  return [classe1, notclasse1];
 }
 export function getActive() {
-  const activeclasse1 = document.querySelectorAll(".classical.active");
-  const notactiveclasse1 = document.querySelectorAll(
-    ":not(.classical.active)"
-  );
-  //console.log(activeclasse1,notactiveclasse1);
+  // console.log("ttttttttttttttt");
 
-  return [...activeclasse1], [...notactiveclasse1];
+  const activeclasse1 = document.querySelectorAll("a.classical.active");
+  const notactiveclasse1 = document.querySelectorAll(
+    ":not(a.classical.active)"
+  );
+  console.log(activeclasse1, notactiveclasse1);
+
+  return [activeclasse1, notactiveclasse1];
 }
 export function getBonannoPisano() {
   const BonannoPisano = document.getElementById("BonannoPisano");
   const dsd = document.querySelectorAll(":not(#BonannoPisano)");
-  return BonannoPisano, dsd;
+  return [BonannoPisano, dsd];
 }
