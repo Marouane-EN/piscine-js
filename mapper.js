6a4bfbe0465010b46458ce8d9eb684893ac4be60
soubaaiss <aissoulhcen@gmail.com>
2025-06-28 10:54:29 +0100

    final commit

--- mapper.js
+++ mapper.js
@@ -5,3 +5,15 @@ const map = (array, func) => {
   }
   return arr;
 };
+
+const flatMap = (array, func) => {
+  const arr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(func(array[i], i, array))) {
+      arr.push(...func(array[i], i, array));
+    } else {
+      arr.push(func(array[i], i, array));
+    }
+  }
+  return arr;
+};
