4dda47929b2ecbb996cb92be531d5c83cfe122f0
mennaas <mennaas@example.com>
2025-06-29 11:59:06 +0100

    final commit

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,37 +1,27 @@
-import { styles } from "./pimp-my-style.data.js"
+import { styles } from "./pimp-my-style.data.js";
 
-let count = 0
-let decrease = false
+let count = 0;
+let decrease = false;
 export function pimp() {
-    const button = document.querySelector(".button")
-    if (count == styles.length) {
-        // console.log("11111");
-        // console.log(button);
+  const button = document.querySelector(".button");
+  if (decrease && count < 0) {
+    button.classList.remove("unpimp");
+    count = 0;
+    decrease = false;
+    return;
+  }
+  if (decrease) {
+    button.classList.remove(styles[count]);
+    --count;
+    return;
+  }
 
-        button.classList.add("unpimp")
-        --count
-        decrease = true
-        return
-    }
-    if (decrease && count < 0) {
-        // console.log("2222222222");
-        // console.log(button);
-
-        button.classList.remove("unpimp")
-        count = 0
-        decrease = false
-        return
-    }
-    if (decrease) {
-        // console.log("33333333333");
-        // console.log(button);
-
-        button.classList.remove(styles[count])
-        --count
-        return
-    }
-    // console.log(button);
-
-    button.classList.add(styles[count])
-    ++count
+  button.classList.add(styles[count]);
+  ++count;
+  if (count == styles.length) {
+    button.classList.add("unpimp");
+    --count;
+    decrease = true;
+    return;
+  }
 }
